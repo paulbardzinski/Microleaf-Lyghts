0d98aa769f073357700d3cd6682f2f910ce6277a
paulbardzinski <bardzinskipaul@gmail.com>
2025-04-03 17:09:38 +0200

    Add files via upload

---
+++ src/esp32-controller/include/data.h
@@ -0,0 +1,14 @@
+#ifndef DATA_H
+#define DATA_H
+
+// This file contains the data that is used by the program
+
+#define NETWORK_SSID "MyName";      // your network SSID (name)
+#define NETWORK_PASSWORD "password"; // your network password
+
+#define LED_PIN 4
+#define NUM_LEDS 54
+#define NUM_OF_SECTORS 9
+// #define NUM_LEDS 39
+
+#endif
